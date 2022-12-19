2211f503fd11f295abe859dcb5fc85f7bc7f4de6
Lucas0105 <dnjswo105@gmail.com>
2022-12-19 22:39:40 +0900

    init: custom accounts

--- navi_backend/config/settings.py
+++ navi_backend/config/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # apps
+    'accounts',
+
+    # third prty apps
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
