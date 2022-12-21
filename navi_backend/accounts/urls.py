6eb6bf6ddafad85893295a62e1510842940bd395
appletail <csappletail@gmail.com>
2022-12-21 21:58:34 +0900

    feat: requests, django-environ freeze, 환경변수 분리 (#9)

--- navi_backend/accounts/urls.py
+++ navi_backend/accounts/urls.py
@@ -1,4 +1,6 @@
+from django.urls import path, include
+from accounts import views
 
 urlpatterns = [
-
+    # path('', views.home, name='home')
 ]
--- navi_backend/config/settings.py
+++ navi_backend/config/settings.py
@@ -9,21 +9,28 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
+import environ
 
 from pathlib import Path
+
+env = environ.Env(DEBUG=(bool, True))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# 환경 변수 설정
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u)mjyh(oqm(a3o9tbseav1@1aghl3eg1wyp45j(#l!0m@kvc1p'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
