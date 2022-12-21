70052d4ab07d143c33f24d03ff86bec30afe8b0f
Lucas0105 <dnjswo105@gmail.com>
2022-12-21 20:20:55 +0900

    feat: 구름과 강수형태를 알 수 있는 weather 기능 추가 (#8)

--- navi_backend/themes/urls.py
+++ navi_backend/themes/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create),
+
+    path('weather/<str:lat>/<str:lon>/', views.weather),
 ]
--- navi_backend/themes/views.py
+++ navi_backend/themes/views.py
@@ -1,6 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import ThemeCreateSerializer
+from .weather import getWeather
+
 
 @api_view(['POST'])
 def create(request):
@@ -14,4 +16,14 @@ def create(request):
         'theme_id': theme.id,
     }
     return Response(data)
+
+
+@api_view(['GET'])
+def weather(request, lat, lon):
+
+    # cloud: 구름의 양, precipitation: 강수형태,  lat: 위도, lon: 경도
+    cloud, precipitation = getWeather(float(lat), float(lon))
+
+    # cloud
+    print(cloud, precipitation)
 
