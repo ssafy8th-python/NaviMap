0ae0c8383bbc469f834673c1d2a321a1a09db1a3
cygyeong <cyellowgreen@gmail.com>
2022-12-24 12:02:16 +0900

    feat: 장소 추가 레이아웃 구현(#27)

---
+++ navi_frontend/navimap/src/components/place-detail/memory-log-form/MemoryLogForm.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<TheMemories
+		:memories="memories"
+		/>
+		<h3>추억로그 남기기</h3>
+		<MemoryInput
+		@getReviewContent='getContentF'
+		/>
+	</div>
+</template>
+
+<script>
+import MemoryInput from '../memory-log-form/MemoryInput.vue'
+import TheMemories from '../memory-log-form/TheMemories.vue'
+
+export default {
+	name:'MemoryLogForm',
+	components:{
+		MemoryInput,
+		TheMemories
+	},
+	data() {
+		return{
+			memories:[],
+		}
+	},
+	methods:{
+		getContentF(newInput){
+			this.memories.push(newInput)
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
