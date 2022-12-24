e11ffb2c17da26bfa0c8d5aa02811fe3214a5a80
appletail <csappletail@gmail.com>
2022-12-24 20:21:07 +0900

    feat: user model 작성, 사용자 카카오 프로필 수집 구현

--- navi_backend/accounts/models.py
+++ navi_backend/accounts/models.py
@@ -1,4 +1,9 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    nickname = models.CharField(max_length=100, null=True)
+    profile_image = models.TextField(blank=True)
+    thumbnail_image = models.TextField(blank=True)
+    email = models.EmailField(max_length=254)
+    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
--- navi_backend/accounts/views.py
+++ navi_backend/accounts/views.py
@@ -9,6 +9,7 @@ from django.conf import settings
 
 @api_view(['GET'])
 def kakaoGetcode(request):
+    # Step 1: 인가 코드 받기
     client_id = settings.KAKAO_APP_CONFIG.get('REST_API_KEY')
     redirect_uri = settings.KAKAO_APP_CONFIG.get('REDIRECT_URI')
     url = f'https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code'
@@ -18,6 +19,8 @@ def kakaoGetcode(request):
 @api_view(['GET'])
 def getUserInfo(request):
     KAKAO_APP_CONFIG = settings.KAKAO_APP_CONFIG
+
+    # Step 2: 토큰 받기
     auth_code = request.GET.get('code')
     kakao_token_api = 'https://kauth.kakao.com/oauth/token'
     throw_token_data = {
@@ -28,4 +31,25 @@ def getUserInfo(request):
         'client_secret': KAKAO_APP_CONFIG.get('CLIENT_SECRET'),
     }
     token_response = requests.post(kakao_token_api, data=throw_token_data)
-    return Response(token_response)
+
+    # Step 3: 사용자 로그인 처리
+    kakao_userinfo_api = 'https://kapi.kakao.com/v2/user/me'
+    access_token = token_response.json().get('access_token')
+    kakao_userinfo_headers = {'Authorization': f'Bearer {access_token}'}
+
+    user_info = requests.get(kakao_userinfo_api, headers=kakao_userinfo_headers)
+
+    # 회원가입
+
+    # 로그인
+    
+    return Response(user_info.json())
+
+# token_response = {
+# "access_token": "1hmix1GjNob4qeMCo-y8FF_i-FBTntkGFai10x78Cj102wAAAYU-Cdhr",
+# "token_type":"bearer",
+# "refresh_token":"RgjzP6UNWbyRv8IXjwsi2VuE82BdePirzLCZ_29RCj102wAAAYU-Cdhp",
+# "expires_in":21599,
+# "scope":"account_email profile_image profile_nickname",
+# "refresh_token_expires_in":5183999
+# }
