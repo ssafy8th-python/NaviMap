ecdb66e3eb3932453a3d468ef23f46cf5222d843
Sungwook Cho <saeloun8797@gmail.com>
2022-12-29 13:27:16 +0900

    feat: 테마상세 페이지 카카오맵 지도 구현(#1)

---
+++ navi_frontend/navimap/src/components/map/KakaoMap.vue
@@ -0,0 +1,39 @@
+<template>
+  <div ref="map">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'KakaoMap',
+    props:{
+        options: Object
+    }, 
+    mounted() {
+        let kakao = window.kakao
+        console.log(this.$refs.map)
+
+        const container = this.$refs.map
+        // const options = {
+        //     center: new kakao.maps.LatLng(35.193681, 126.825577, 16),
+        //     level: 5,
+        // }
+        const {center, level} = this.options
+        const mapInstance = new kakao.maps.Map(container, {
+            center: new kakao.maps.LatLng(center.lat, center.lng),
+            level: level,
+        })
+        console.log(mapInstance)
+    }
+
+}
+</script>
+
+<style>
+.kmap {
+    width: 100%;
+    height: 600px;
+}
+
+</style>
