9ae83275e701ee1253ee4729b3be56545a1fb898
Sungwook Cho <saeloun8797@gmail.com>
2022-12-29 13:27:16 +0900

    feat: 테마상세 페이지 카카오맵 지도 구현(#11)

--- navi_frontend/navimap/src/router/index.js
+++ navi_frontend/navimap/src/router/index.js
@@ -1,23 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ThemeDetailView from '@/views/ThemeDetailView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  
   {
     path: '/',
     name: 'home',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path:"/themedetail",
+    name: 'ThemeDetailView',
+    component: ThemeDetailView
+  },
+
 ]
 
 const router = new VueRouter({
---
+++ navi_frontend/navimap/src/views/ThemeDetailView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h3>ThemeDetailView</h3>
+        <KakaoMap class="kmap" :options="mapOption"/>
+    </div>
+</template>
+
+<script>
+import KakaoMap from '@/components/map/KakaoMap'
+
+export default {
+    name:'ThemeDetailView',
+    components: {
+        KakaoMap
+    },
+    data() {
+        return{
+            mapOption:{
+                center: {
+                    lat: 35.193681,
+                    lng: 126.825577,
+                },
+                level : 8
+            }
+        }
+    },
+    mounted() {
+    
+    },
+
+}
+</script>
+
+<style>
+
+</style>
