37b8c63f10e8dfad2117f840818373e121ed0755
appletail <csappletail@gmail.com>
2023-01-08 15:55:52 +0900

    fix: account urls.py 주석 & url 수정 (#51)

--- navi_backend/accounts/urls.py
+++ navi_backend/accounts/urls.py
@@ -4,8 +4,8 @@ from accounts import views
 
 
 urlpatterns = [
-    path('kakao/login/getcode/', views.kakaoGetcode),  # 카카오 로그인
-    path('kakao/login/redirect_uri/', views.login),  # 카카오 로그인 내부 로직용
+    path('kakao/login/getcode/', views.kakaoGetcode),  # 카카오 코드 요청
+    path('kakao/login/gettoken/', views.login),  # 카카오 로그인 요청
     path('kakao/delete/', views.kakaoDelete),  # 카카오 회원탈퇴
     path('logout/', views.logout),  # 카카오 회원탈퇴
 ]
